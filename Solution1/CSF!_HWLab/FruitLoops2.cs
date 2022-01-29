159efd0fa9f05daa60d4ed48b3de3a8c7e9522a9
Zakary Sisler <zakarysisler@outlook.com>
2022-01-28 19:48:28 -0600

    I matched periods to each fruit and displayed it on the console

---
+++ Solution1/CSF!_HWLab/FruitLoops2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF__HWLab
+{
+    class FruitLoops2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fruit Loops 2");
+
+            string[] fruit = { "apples\n .", "oranges\n..", "blueberries\n...", "bananas\n....", "watermelons\n.....", "kiwis\n......" };
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.WriteLine(fruit[i]);
+            }
+
+        }
+    }
+}
