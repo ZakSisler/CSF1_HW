2f7028c51f571f9d4e1430128395a6e1ae731893
Zakary Sisler <zakarysisler@outlook.com>
2022-01-29 12:52:05 -0600

    I made a do while loop and messed around with  nesting if/else and for loops within it.  I have a slight bug on line 44 that I'll need to fix that outputs information that should only be read when access is denied to the console.

--- Solution1/CSF!_HWLab/Class_Login.cs
+++ Solution1/CSF!_HWLab/Class_Login.cs
@@ -12,24 +12,49 @@ namespace CSF__HWLab
         {
             Console.WriteLine("Class: Login");
 
+            bool correctUser = false;
+
+
             Console.WriteLine("What's your username: ");
             string username = Console.ReadLine().ToLower();
 
-            if (username == "correct username")
+
+            do
             {
-                Console.WriteLine("Correct username!");
-            }
-            else if (username != "correct username")
-            {
-                Console.WriteLine("That is the wrong username!");
-            }
 
-            Convert.ToInt16(username);
-            Console.WriteLine("Enter your username again");
+                if (username == "correct username" || username == "admin")
+                {
+                    correctUser = true;
+                    Console.WriteLine("Access Granted!");
+                }
+                else if (username != "correct username")
+                {
+                    Console.WriteLine("That is the wrong username!\n ACCESS DENIED - you have 2 more tries remaining...");
+                    for (int i = 1; i < 3; i++)
+                    {
+                        Console.WriteLine("Enter your username again");
+                        username = Console.ReadLine().ToLower();
+                        if (username == "correct username" || username == "admin")
+                        {
+                            correctUser = true;
+                            Console.WriteLine("Access Granted!");
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Permission denied. Terminal locked.");//Fix this to not execute if permission is granted
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You are not admin");
+                }
 
-            //For loop here?
-            //Put the whole thing in a DoWhile loop?
+                //Convert.ToInt16(username);
+                //Console.WriteLine("Enter your username again");
 
+                //For loop here?
+                //Put the whole thing in a DoWhile loop?
+            } while (!correctUser);
 
         }
     }
