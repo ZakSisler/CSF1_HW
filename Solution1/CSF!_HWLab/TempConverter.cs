81f40039a1d22c494409bf05a397ee70c4998161
Zakary Sisler <zakarysisler@outlook.com>
2022-01-28 22:15:07 -0600

    I made a temp converter with a menu using a do while loop and a switch case.

--- Solution1/CSF!_HWLab/TempConverter.cs
+++ Solution1/CSF!_HWLab/TempConverter.cs
@@ -10,7 +10,54 @@ namespace CSF__HWLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Temp Converter");
+
+            bool repeat = true;
+
+            do
+            {
+
+
+                Console.WriteLine();
+                Console.WriteLine("Temp Converter");
+
+                Console.WriteLine("\n\nPlease choose a Temperature:\n" +
+                   "1) Celsius converted to Fahrenheit: Press \"A\"\n" +
+                   "2) Fahrenheit converted to Celsius: Press \"B\"\n");
+
+                ConsoleKey userChoice = Console.ReadKey(true).Key;
+                Console.Clear();
+
+                switch (userChoice)
+                {
+                    case ConsoleKey.A:
+                        Console.Write("What is the current temperature in Celsius? ");
+                        int userCelInput = Convert.ToInt16(Console.ReadLine());
+                        int toFahConversion = userCelInput * 9 / 5 + 32;
+                        Console.WriteLine("Celsius is " + toFahConversion);
+                        break;
+                    case ConsoleKey.B:
+                        Console.Write("What is the current temp in Fahrenheit? ");
+                        int userFahInput = Convert.ToInt16(Console.ReadLine());
+                        int toCelConversion = (userFahInput - 32) * 5 / 9;
+                        Console.WriteLine("Fahrenheit is " + toCelConversion);
+                        break;
+                        
+                    default:
+                        Console.Write("ERROR: INVALID SELECTION");
+                        break;
+                }
+
+            } while (repeat);
+
         }
     }
 }
+//Console.WriteLine("What is the current temperature in Celsius? ");
+//int userCelInput = Convert.ToInt16(Console.ReadLine());
+
+//int toFahConversion = userCelInput * (9 / 5) + 32;
+
+//Console.WriteLine("What is the current temp in Fahrenheit? ");
+//int userFahInput = Convert.ToInt16(Console.ReadLine());
+
+//int toCelConversion = (userFahInput - 32) / (9 / 5);
