6d8a85a7c577b44a40ceb066192b5d7b9e69d914
Zakary Sisler <zakarysisler@outlook.com>
2022-01-28 20:20:13 -0600

    initial commit - I also created a madlib and used string formatting to piece a story together.

---
+++ Solution1/CSF!_HWLab/MadLibs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF__HWLab
+{
+    class MadLibs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mad Libs!");
+
+            Console.WriteLine("Give me a noun: ");
+            string season = Console.ReadLine();
+            Console.WriteLine("A verb: ");
+            string car = Console.ReadLine();
+            Console.WriteLine("An adjective: ");
+            string show = Console.ReadLine();
+            Console.WriteLine("A silly name: ");
+            string cash = Console.ReadLine();
+
+            Console.WriteLine("\nThe {0} {1} and {2} called you {3} as you were reading this... \n\nYeah, this was a short story.\n", season, car, show, cash);
+
+        }
+    }
+}
